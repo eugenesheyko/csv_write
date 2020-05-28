d0a3b8576f752da1e54b1768a8edbe251445f649
Eugene Sheyko <eugene.sheyko@yahoo.com>
2020-05-28 14:45:32 -0400

    Update Print statements

--- csv_write.py
+++ csv_write.py
@@ -5,11 +5,8 @@ with open('employee_birthday.txt', mode='r') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            # print(f'Column names are {", ".join(row)}')
             print('Column names are %s'%", ".join(row))
-            #print('Column names are {}'.format(", ".join(row)))
             line_count += 1
-        #print(f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.')
-        print('\t{} works in the {} department, and was born in {}.'.format(join(row)))
+        print('\t'+row['name']+'works in the '+row['department']+', and was born in '+row['birthday month'])
         line_count += 1
-    print(f'Processed {line_count} lines.')
+    print('Processed {} lines.'.format(line_count))
